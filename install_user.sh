8407459ea18f90b1af92350f77fcd3e3d0cf088a
guanadoo <guanadoo@aol.com>
2025-12-27 17:30:52 +0100

    Create install_user.sh

---
+++ install_user.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# SCRIPT GITHUB + DEPLOY
+
+# intalación gh client
+
+(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+
+# echo API de acceso a git
+read -p 'token: ' tokenvar
+export GH_TOKEN=$tokenvar
+gh auth login --with-token $GH_TOKEN
+
+# comprobar token exportado
+# gh config get -h github.com oauth_token
+
+# clonar repo deploy
+read 'gituser: ' gitvar
+read 'repo: ' repovar
+echo clon: $gitvar/$repovar
+gh repo clone $gitvar/$repovar
+
+# iniciar deploy
+chmod +x $repovar/install.sh
+$repovar/install.sh

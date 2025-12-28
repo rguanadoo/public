806280cb9a6d1e8a1a78402c76c0f9a50136878d
guanadoo <guanadoo@aol.com>
2025-12-28 02:25:36 +0100

    Update install_user.sh

--- install_user.sh
+++ install_user.sh
@@ -12,8 +12,7 @@ echo introduce el repositorio
 read -p 'repo: ' gitrepo
 
 # intalación gh client
-sudo apt install gh
-apt install net-tools rsync nano vi telnet curl git iputils-ping  --yes
+sudo apt install net-tools rsync nano vi telnet curl git iputils-ping gh --yes
 
 # echo API de acceso a git
 # read -sp 'token: ' tokenvar

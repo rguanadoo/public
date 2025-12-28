7016265c2b9c7f6dd961ca1d362628f9488a1799
guanadoo <guanadoo@aol.com>
2025-12-28 22:46:29 +0100

    Update install_root.sh

--- install_root.sh
+++ install_root.sh
@@ -4,16 +4,17 @@
 
 # variables
 echo usuario y password administrador
-read -p 'usuario: ' uservar
-read -sp 'password: ' passvar
-echo Usuario admin del sistema:  $uservar
+$ipcli=(hostname -I | cut -d' ' -f1)
+read -p 'usuario: ' useradm
+read -sp 'password: ' userpass
+echo Usuario admin del sistema:  $useradm
 
 # update os
 apt update && apt upgrade --yes
 apt install sudo rsync nano telnet curl net-tools wget git --yes
 
 # usuario admin + reinicio
-useradd -m -s /bin/bash $uservar -p $passvar
-usermod -aG sudo $uservar
-echo nuevo administrador: $uservar
-shutdown -r +1 "################## REINICIANDO EL SISTEMA... ##################"
+useradd -m -s /bin/bash $useradm -p $userpass
+usermod -aG sudo $useradm
+echo acceso ssh: 'ssh '$useradm'@'$ipcli''
+shutdown -r +1 "################## Reiniciando el sistema... ##################"

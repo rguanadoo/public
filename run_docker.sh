47ebab03dcf432057e530430dcdb1ee17e29000e
guanadoo <guanadoo@aol.com>
2025-12-30 01:53:33 +0100

    Create run_docker.sh

---
+++ run_docker.sh
@@ -0,0 +1,44 @@
+# DOCKER ENGINE
+sudo curl https://get.docker.com | sh
+
+# permisos usuario docker
+sudo usermod -aG docker $USER
+
+# LAZYDOKER
+#!/bin/bash
+# allow specifying different destination directory
+DIR="/usr/local/bin"
+
+# map different architecture variations to the available binaries
+ARCH=$(uname -m)
+case $ARCH in
+    i386|i686) ARCH=x86 ;;
+    armv6*) ARCH=armv6 ;;
+    armv7*) ARCH=armv7 ;;
+    aarch64*) ARCH=arm64 ;;
+    amd64*) ARCH=x86_64 ;;
+esac
+
+# prepare the download URL
+GITHUB_LATEST_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/jesseduffield/lazydocker/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+GITHUB_FILE="lazydocker_${GITHUB_LATEST_VERSION//v/}_$(uname -s)_${ARCH}.tar.gz"
+GITHUB_URL="https://github.com/jesseduffield/lazydocker/releases/download/${GITHUB_LATEST_VERSION}/${GITHUB_FILE}"
+
+# install/update the local binary
+curl -L -o lazydocker.tar.gz $GITHUB_URL
+tar xzvf lazydocker.tar.gz lazydocker
+sudo install -Dm 755 lazydocker -t "$DIR"
+rm lazydocker lazydocker.tar.gz
+
+# DOCKER-COMPOSE
+#!/bin/sh
+set -e
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+# REINICIO
+export ipcli=$( hostname -I | cut -d' ' -f1)
+echo reinicio permisos: 'ssh '$USER'@'$ipcli''
+sudo shutdown -r +1 "################## Reiniciando el sistema... ##################"
+

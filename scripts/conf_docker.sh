c93ca4d0d7964cea5a608e8e6c7b125bd6dfa128
guanadoo <guanadoo@aol.com>
2026-01-29 16:04:02 +0100

    Update conf_docker.sh

--- scripts/conf_docker.sh
+++ scripts/conf_docker.sh
@@ -38,7 +38,7 @@ rm lazydocker lazydocker.tar.gz
 # DOCKER-COMPOSE
 #!/bin/sh
 set -e
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/v5.0.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
